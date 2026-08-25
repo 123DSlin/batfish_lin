aab08cc93e8c0462f2a11693c4ef3f933f653e18
123DSlin <2451753232@qq.com>
2026-08-25 22:22:03 +0800

    test(minesweeper): complete stage 4.3 static policy coverage [2026-08-25 22:21 CST]

--- projects/minesweeper/src/test/java/org/batfish/minesweeper/symbolicroute/BatfishRoutingPolicyProcessorTest.java
+++ projects/minesweeper/src/test/java/org/batfish/minesweeper/symbolicroute/BatfishRoutingPolicyProcessorTest.java
@@ -1,12 +1,16 @@
 package org.batfish.minesweeper.symbolicroute;
 
 import static org.batfish.datamodel.ConfigurationFormat.CISCO_IOS;
+import static org.batfish.datamodel.routing_policy.Environment.Direction.IN;
 import static org.batfish.datamodel.routing_policy.Environment.Direction.OUT;
 import static org.batfish.datamodel.routing_policy.statement.Statements.ExitAccept;
 import static org.batfish.datamodel.routing_policy.statement.Statements.ExitReject;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Collections;
+import org.batfish.datamodel.AbstractRoute;
 import org.batfish.datamodel.AnnotatedRoute;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.NetworkFactory;
@@ -90,5 +94,101 @@ public final class BatfishRoutingPolicyProcessorTest {
 
     assertThat(result.getOutcome(), equalTo(BatfishRoutingPolicyResult.Outcome.POLICY_NOT_FOUND));
     assertThat(result.getOutputRoute().isPresent(), equalTo(false));
+  }
+
+  @Test
+  public void testModificationBeforeRejectDoesNotLeakOrMutateInput() {
+    RoutingPolicy.builder()
+        .setOwner(_configuration)
+        .setName("modify-then-reject")
+        .addStatement(new SetMetric(new LiteralLong(999L)))
+        .addStatement(ExitReject.toStaticStatement())
+        .build();
+    StaticRoute original = _input.getRoute();
+
+    BatfishRoutingPolicyResult<StaticRoute> result =
+        BatfishRoutingPolicyProcessor.process(
+            _configuration,
+            "modify-then-reject",
+            _input,
+            _input.getRoute().toBuilder(),
+            OUT,
+            "target");
+
+    assertThat(result.getOutcome(), equalTo(BatfishRoutingPolicyResult.Outcome.DENIED));
+    assertThat(result.getOutputRoute().isPresent(), equalTo(false));
+    assertThat(_input.getRoute(), equalTo(original));
+    assertThat(_input.getRoute().getMetric(), equalTo(10L));
+  }
+
+  @Test
+  public void testInDirectionExactlyMatchesDirectBatfishExecution() {
+    RoutingPolicy policy =
+        RoutingPolicy.builder()
+            .setOwner(_configuration)
+            .setName("import")
+            .addStatement(new SetMetric(new LiteralLong(70L)))
+            .addStatement(ExitAccept.toStaticStatement())
+            .build();
+    StaticRoute.Builder directBuilder = _input.getRoute().toBuilder();
+    assertThat(policy.process(_input, directBuilder, IN), equalTo(true));
+
+    BatfishRoutingPolicyResult<StaticRoute> result =
+        BatfishRoutingPolicyProcessor.process(
+            _configuration, "import", _input, _input.getRoute().toBuilder(), IN, "target");
+
+    assertThat(result.getOutputRoute().get().getRoute(), equalTo(directBuilder.build()));
+  }
+
+  @Test
+  public void testAcceptedPolicyResultIntegratesWithSymbolicGuardedRib() {
+    RoutingPolicy.builder()
+        .setOwner(_configuration)
+        .setName("symbolic-export")
+        .addStatement(new SetMetric(new LiteralLong(80L)))
+        .addStatement(ExitAccept.toStaticStatement())
+        .build();
+    BatfishRoutingPolicyResult<StaticRoute> policyResult =
+        BatfishRoutingPolicyProcessor.process(
+            _configuration,
+            "symbolic-export",
+            _input,
+            _input.getRoute().toBuilder(),
+            OUT,
+            "target");
+    SymbolicRouteNetwork<AnnotatedRoute<AbstractRoute>> network =
+        SymbolicRouteNetworkFactory.create(
+            ImmutableList.of("r1"),
+            Collections.emptyList(),
+            Collections.emptyList(),
+            new BatfishMainRibRouteAdapter());
+    RouteGuard guard =
+        new Z3RouteGuardFactory(new com.microsoft.z3.Context()).variable("policy_guard");
+    BatfishRedistributionKey key =
+        new BatfishRedistributionKey(
+            "source",
+            "r1",
+            "source",
+            "target",
+            org.batfish.datamodel.RoutingProtocol.STATIC,
+            org.batfish.datamodel.RoutingProtocol.STATIC,
+            "symbolic-export");
+
+    new BatfishRedistributionReconciler(network).reconcile(key, policyResult, guard);
+
+    assertThat(network.getRib("r1").getEntries().size(), equalTo(1));
+    assertThat(
+        network.getRib("r1").getEntries().get(0).getAvailabilityGuard().isEquivalentTo(guard),
+        equalTo(true));
+    assertThat(
+        network
+            .getRib("r1")
+            .getEntries()
+            .get(0)
+            .getSymbolicRoute()
+            .getRoute()
+            .getAbstractRoute()
+            .getMetric(),
+        equalTo(80L));
   }
 }
